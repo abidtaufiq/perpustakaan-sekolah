9e11c839082195b2a04cb610983f11d0d44f9692
Abid Taufiqur Rohman <abid5200411064@student.uty.ac.id>
2022-08-10 11:08:43 +0700

    first commit

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>. FA Team</strong> PT Pacifica Raya Technology. | Repost by <a href='https://sharecodingan.blogspot.com/' title='sharecodingan.blogspot.com' target='_blank'>ShareCoding</a>
+    
+</footer>
